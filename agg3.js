8ffee63148a3ee660dc67531dda1be382a8eb3c9
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-11-29 00:54:39 +0100

    fixed aggreagation and changed pipes to variables

--- agg3.js
+++ agg3.js
@@ -1,11 +1,18 @@
 var connection = new Mongo();
 var db = connection.getDB('getglue');
 
+var match1 = { $match: {"comment": {$ne: ""}} };
+var group = { $group: {"_id": "$userId", "total": {$sum: 1}} };
+var match2 = { $match: {"total": {$gte: 5000}} };
+var sort = { $sort: {total: -1} };
+var limit = { $limit: 7 };
+
 var results = db.gg.aggregate(
-	{ $group: {_id: "$userId", total: {$sum: 1}} },
-	{ $match: {total: {$gte: 50000}} },
-	{ $sort: {total: -1} },
-	{ $limit: 7 }
+	match1,
+	group,
+	match2,
+	sort,
+	limit
 );
 
 printjson(results);
