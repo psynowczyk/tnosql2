bf038fa6c58e3a49a9426eef5acefcf150d84118
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-11-29 01:40:00 +0100

    fixed match pipe to avoid director field with 'not available' or 'various directors' values

--- agg2.js
+++ agg2.js
@@ -1,7 +1,7 @@
 var connection = new Mongo();
 var db = connection.getDB('getglue');
 
-var match = { $match: {"modelName": "movies" || "tv_shows"} };
+var match = { $match: {"modelName": "movies" || "tv_shows", $and: [{"director": {$ne: "not available"}}, {"director": {$ne: "various directors"}}]} };
 var group1 = { $group: {"_id": {"director": "$director", "title": "$title"}, "total": {$sum: 1}} };
 var group2 = { $group: {"_id": "$_id.director", "total": {$sum: 1}} };
 var sort = { $sort: {total: -1} };
